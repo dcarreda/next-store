03a19fcc10acaadd36fb980aa6463ecacec193fb
diogoarreda <diogo.arreda@bsb-ai.com>
2021-05-13 14:31:58 +0100

    ready for deploy

--- functions/charge-cart.js
+++ functions/charge-cart.js
@@ -1,26 +1,9 @@
-const fs = require("fs");
-const matter = require("gray-matter");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const products = require("./products.json")
 
-const getProducts = () => {
-    const directory = `${process.cwd()}/content`;
-    const filenames = fs.readdirSync(directory);
-
-    const products = filenames.map((filename) => {
-        // read the file from fs
-        const fileContent = fs.readFileSync(`${directory}/${filename}`).toString();
-        // pull out frontmatter => name
-        const { data } = matter(fileContent);
-
-        return data;
-    });
-
-    return products;
-};
 
 exports.handler = async (event, context) => {
     const cart = JSON.parse(event.body);
-    const products = getProducts();
 
     const cartWithProducts = cart.newCart.map(({ id, qty }) => {
         const product = products.find((p) => p.id === id);
